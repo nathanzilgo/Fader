7735ca7740c98803a29b0ca72eb35ae2e065bf9d
my name <nathanfpedroza@gmail.com>
2018-12-03 16:35:53 -0300

    ...

--- src/fader.py
+++ src/fader.py
@@ -3,14 +3,16 @@ import numpy as np
 from matplotlib import pyplot as plt
 from time import sleep
 
-imagem1 = cv.imread("thiago.jpeg", 1)
 
-#plt.hist(imagem1.ravel(), 256, [0,256])
-#plt.show()
+imagem1 = cv.imread("gandalf.jpg", 1)
+imagem2 = cv.imread("gandalfrosa.jpeg", 1)
 
-for x in range(0, 10000):
-    cv.imshow("imagem1", imagem1 * x)
-    cv.waitKey(0)
+img1 = cv.resize(imagem1, (400,400))
+img2 = cv.resize(imagem2, (400,400))
+
+novaimg = img1 + img2
+
+cv.imshow("imagens", novaimg)
 
 cv.waitKey(0)
 cv.destroyAllWindows()
